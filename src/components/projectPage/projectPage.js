6f00c77bc3ab1e9e66a737f66c5144d1b65989bd
Eliza1991 <aghasyaneliza@gmail.com>
2021-08-01 00:10:39 +0400

    Todo App - Project page's content is created

---
+++ src/components/projectPage/projectPage.js
@@ -0,0 +1,11 @@
+const ProjectPage = () => {
+    return (
+        <div className="projectPage">
+            <h2 className="">Welcome to our Todo App</h2>
+            <p>Here you can create your first project</p>
+            <button>Create Project</button>
+        </div>
+    );
+}
+
+export default ProjectPage;
