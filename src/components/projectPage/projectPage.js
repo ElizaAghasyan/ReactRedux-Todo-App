39ad563bb107a35382c5c96a9287668b78ea3e37
Eliza1991 <aghasyaneliza@gmail.com>
2021-08-02 13:03:56 +0400

    node-sass files are deleted

--- src/components/projectPage/projectPage.js
+++ src/components/projectPage/projectPage.js
@@ -4,7 +4,7 @@ import './projectPage.scss';
 const ProjectPage = () => {
     return (
         <div className="projectPage">
-            <h2 className="projectPage-title">You can create your projects here</h2>
+            <h2 className="projectPage-title">YOU CAN CREATE YOUR PROJECTS HEREe</h2>
             <Button variant="outlined" className="btn">Create Project</Button>
         </div>
     );
